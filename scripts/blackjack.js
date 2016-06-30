3a34219f76330c8a381dd32d2cd3ae340796e075
RyFisher <ryanf@passfail.com>
2016-06-30 17:10:31 -0500

    New user logic edit

--- scripts/blackjack.js
+++ scripts/blackjack.js
@@ -18,26 +18,22 @@ var users = [];
 module.exports = function(robot) {
 	robot.respond(/deal/i, function(msg) {
 		var user = {};
-		user.name = msg.message.user.name;
-		user.hand = Deck.deal();
-		user.action = "";
 		
-		//Add user to game
-		if(users.length == 0){
+		//Check for new user
+		var newUser = true;
+		for(var i = 0; i < users.length; i++){
+			if(users[i] == user){
+				newUser = false;
+				msg.send('You are already playing!');
+			}
+		}
+		if(newUser){
+			user.name = msg.message.user.name.toLowerCase();
+			user.hand = Deck.deal();
+			user.action = "";
 			users.push(user);
 		}
-		else{
-			var newUser = true;
-			for(var i = 0; i < users.length; i++){
-				if(users[i] == user){
-					newUser = false;
-				}
-			}
-			if(newUser){
-				users.push(user);
-			}
-		}
+	}		
 		
-		
-		msg.send('');
+	msg.send('');
 	});
