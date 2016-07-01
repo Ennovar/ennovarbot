229a61d5f3fe66f7feee463cadcd9093567cdd3c
RyFisher <ryanf@passfail.com>
2016-07-01 12:47:02 -0500

    Code cleanup, logic prototype

--- scripts/blackjack.js
+++ scripts/blackjack.js
@@ -1,50 +1,3 @@
-
-/*
-Game setup functions
- */
-// setup deck of cards
-function getCards(suits, ranks) {
- var cards = [];
- // for each suit create all of the ranks cards
- for (var i = 0; i < suits.length; i++) {
-   var suit = suits[i];
-   // for each rank create a card for it
-   for (var j = 0; j < ranks.length; j++) {
-     var rank = ranks[j];
-     // create a json object to represent our cards with a suit and a rank
-     cards.push({suit: suit, rank: rank});
-   }
- }
- return cards
-}
-
-// Card suits
-var suits = [':diamonds:',':hearts:', ':clubs:', ':spades:'];
-// Card ranks 1-K
-var ranks = ['A', 'K', 'Q', 'J', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-var cards = getCards(suits, ranks);
-
-var Deck = {
-  cards: cards,
-  // get numCards from the deck
-  getCards: function(numCards) {
-    var cards = []
-    for (var i = 0; i < numCards; i++) {
-      cards.push(this.cards[Math.floor(Math.random()*this.cards.length)])
-    }
-    return cards;
-  }
-}
-
-var Dealer = {
-  deck: Deck,
-  // Deals two cards from the deck
-  deal: function() {
-    return this.deck.getCards(2)
-  }
-
-}
-
 // Description:
 //   Play Blackjack through hubot
 //
@@ -60,6 +13,55 @@ var Dealer = {
 // Author:
 //   Ryan Fisher and Austin Crane
 
+
+/*
+Game setup functions
+ */
+// setup deck of cards
+function getCards(suits, ranks) {
+    var cards = [];
+    // for each suit create all of the ranks cards
+    for (var i = 0; i < suits.length; i++) {
+        var suit = suits[i];
+        // for each rank create a card for it
+        for (var j = 0; j < ranks.length; j++) {
+            var rank = ranks[j];
+            // create a json object to represent our cards with a suit and a rank
+            cards.push({suit: suit, rank: rank});
+        }
+    }
+    return cards
+}
+
+// Card suits
+var suits = [':diamonds:',':hearts:', ':clubs:', ':spades:'];
+// Card ranks 1-K
+var ranks = ['A', 'K', 'Q', 'J', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+var cards = getCards(suits, ranks);
+
+var Deck = {
+    cards: cards,
+    // get numCards from the deck
+    getCards: function(numCards) {
+        var cards = []
+        for (var i = 0; i < numCards; i++) {
+            cards.push(this.cards[Math.floor(Math.random()*this.cards.length)])
+        }
+        return cards;
+    }
+}
+
+var Dealer = {
+    deck: Deck,
+    // Deals two cards from the deck
+    deal: function() {
+        return this.deck.getCards(2)
+    },
+    hit: function() {
+	    return this.deck.getCards(1)
+    }
+}
+
 var users = [];
 
 module.exports = function(robot) {
@@ -71,13 +73,13 @@ module.exports = function(robot) {
 		for(var i = 0; i < users.length; i++){
 			if(users[i] == user){
 				newUser = false;
-				msg.send('You are already playing!');
+				msg.send('You have already been dealt cards this round!');
 			}
 		}
 		if(newUser){
 			user.name = msg.message.user.name.toLowerCase();
 			user.hand = Dealer.deal();
-			user.action = "";
+			user.status = "canHit";
 			users.push(user);
 			msg.send('Good luck, ' + user.name);
 			console.log(user.hand);
@@ -91,8 +93,23 @@ module.exports = function(robot) {
 			}
 		}
 	});
-  robot.respond(/hand/i, function(msg) {
-    
-    msg.send('');
-  });
+	
+	
+	
+    robot.respond(/hit/i, function(msg) {
+		//check if player is in game and status == canHit
+		
+		//deal 1 card, show hand
+		
+		//set status based on total (canHit or bust)
+		//if busted, display BUSTED message
+        msg.send('');
+    });
+	
+	robot.respond(/stand/i, function(msg) {
+		//check if player is in game and status == canHit
+		
+		//set status to stand
+        msg.send('');
+    });
 }
