6a67379c7837119de8dd7cd562e07fbb01ee0377
Austin Crane <amcrane1@wichita.edu>
2016-07-01 13:23:59 -0500

    created a funciton to get cards from the blackjackapi

--- scripts/blackjack.js
+++ scripts/blackjack.js
@@ -1,50 +1,3 @@
-
-/*
-Game setup functions
- */
-// setup deck of cards
-function getCards(suits, ranks) {
- var cards = [];
- // for each suit create all of the ranks cards
- for (var i = 0; i < suits.length; i++) {
-   var suit = suits[i];
-   // for each rank create a card for it
-   for (var j = 0; j < ranks.length; j++) {
-     var rank = ranks[j];
-     // create a json object to represent our cards with a suit and a rank
-     cards.push({suit: suit, rank: rank});
-   }
- }
- return cards
-}
-
-// Card suits
-var suits = ['♦️','♥️', '♣️', '♠️'];
-// Card ranks 1-K
-var ranks = ['A', 'K', 'Q', 'J', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-var cards = getCards(suits, ranks);
-
-var Deck = {
-  cards: cards,
-  // get numCards from the deck
-  getCards: function(numCards) {
-    var cards = []
-    for (var i = 0; i < numCards; i++) {
-      cards.push(this.cards[Math.floor(Math.random()*this.cards.length)])
-    }
-    return cards;
-  }
-}
-
-var Dealer = {
-  deck: Deck,
-  // Deals two cards from the deck
-  deal: function() {
-    return this.deck.getCards(2)
-  }
-
-}
-
 // Description:
 //   Play Blackjack through hubot
 //
@@ -59,6 +12,34 @@ var Dealer = {
 //
 // Author:
 //   Ryan Fisher and Austin Crane
+
+
+// creates a json out of a json string
+function json(obj) {
+	console.log(obj)
+	return JSON.parse(obj)
+}
+// pretty cards
+function prettyCard(card) {
+	return card.suit + ' ' + card.rank
+}
+
+var blackjack_url = "http://blackjackapi.herokuapp.com";
+var BJAPI = {
+	/**
+	 * deal - gets 2 cards from the dealer, use callback as a callback function to handle recieving data
+	 *
+	 * @param  {function} callback callback as a callback function to handle recieving data, it will have on parameter callback(cards)
+	 * @return {function}          callback
+	 */
+	deal: function(callback) {
+		robot.http(blackjack_url + "/dealer/deal")
+			.get()(function(err, res, body){
+				// return json object with 2 cards
+				return callback(json(body))
+			});
+	},
+};
 
 var users = [];
 
@@ -76,12 +57,12 @@ module.exports = function(robot) {
 		}
 		if(newUser){
 			user.name = msg.message.user.name.toLowerCase();
-			user.hand = Dealer.deal();
 			user.action = "";
 			users.push(user);
-      robot.
 			msg.send('Good luck, ' + user.name);
-      msg.send('First Card ' + user.hand[0].suit + ' ' + user.hand[0].rank)
+      BJAPI.deal(function(cards){
+				msg.send(prettyCard(cards[0]) + prettyCard(cards[1]));
+			})
 			// msg.messageRoom(msg.message.user.name, "Your current hand: ");
 			for(var i = 0; i < user.hand; i++){
 				// msg.messageRoom(msg.message.user.name, user.hand[i]);
@@ -89,7 +70,7 @@ module.exports = function(robot) {
 		}
 	});
   robot.respond(/hand/i, function(msg) {
-    
+
     msg.send()
   });
 }
