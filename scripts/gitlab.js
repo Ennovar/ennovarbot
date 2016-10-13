64fff8b418bd2e95ceaff8e98f11581d98066d6d
Austin Crane <amcrane1@wichita.edu>
2016-10-13 15:54:50 -0500

    added gitlab stuff

---
+++ scripts/gitlab.js
@@ -0,0 +1,22 @@
+// Description:
+//   Play Blackjack through hubot
+//
+// Dependencies:
+//
+//
+// Commands:
+//   hubot deal - Puts you in the current blackjack game.  Starts a new game if there is none.
+//   hubot hit - Adds 1 card to your hand
+//   hubot stand - Holds your current total until the end of the round
+//
+//
+// Author:
+//   Ryan Fisher and Austin Crane
+
+module.exports = function(robot) {
+  robot.router.post("/gitlab/project", function(req, res) {
+    var channel = req.body.channel;
+    var output = req.body.output;
+    robot.messageRoom(channel, output);
+  });
+}
